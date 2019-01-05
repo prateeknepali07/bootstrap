8ee686b336a64385b9b23f92fb7cf1d492aff45e
AkShay Mahajan <vomaksh@gmail.com>
2019-01-06 01:20:48 +0530

    using Bootstrap

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="intro-header ttt">
+        <div className="overlay" />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="site-heading">
+                <h1>Clean Blog</h1>
+                <span className="subheading">
+                  A Blog Theme by Start Bootstrap
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+export default Header;
